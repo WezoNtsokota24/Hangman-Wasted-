fd54cec100cf4be69ffd52664ec9e754f94816c6
Wezo Ntsokota <105015285+WezoNtsokota24@users.noreply.github.com>
2025-09-10 14:46:47 +0200

    Update Hangman.py
    hangman!

--- Hangman.py
+++ Hangman.py
@@ -29,6 +29,8 @@ hangman_steps = {0: ("   ",
                     "/ \\  "),}
 
 #print(hangman_steps[0])
+#for line in hangman_steps[6]:
+#    print(line)
 
 def display_hangman(tries):
     print("####################")
@@ -38,16 +40,19 @@ def display_hangman(tries):
     print("####################")
 
 def display_hint(hint):
-    print
+    print(" ".join(hint))
+    
 
 def display_answer(answer):
-    pass
+    print(" ".join(answer))
 
 
 def main():
     answer= random.choice(words)
     hint = ["_"]*len(answer)
-    tries = 6
+    tries = 0
+
+    
     guessed_letters = set()
     is_runnning = True
     
@@ -55,20 +60,41 @@ def main():
         print("\n")
         display_hangman(tries)
         display_hint(hint)
-        display_answer(answer)
         guess = input("Simon says Guess a letter: ").lower()
+        
+        if len(guess) !=1 or not guess.isalpha():
+            print("Invalid input. Please enter a single letter.")
+            continue
+        
+            
+        if guess in guessed_letters:
+            print(f"{guess} already guessed. Try again.")
+            continue
+        
+        
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+                    
+        
+        else: 
+            tries += 1
+            
+        if "_" not in hint:
+            display_hangman(tries)
+            display_answer(answer)
+            print("Congratulations! You've guessed the word correctly!")
+            is_runnning = False
+        elif tries >= len(hangman_steps)-1:
+            display_hangman(tries)
+            print(f"Sorry, you've run out of tries. The word was '{answer}'.")
+            is_runnning = False
+            
+            
+    
     
     
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
- 
-
-
